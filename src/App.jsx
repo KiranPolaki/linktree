e81372465fc3a100e1cd2d6313b223d3381bf98e
Polaki Sai Kiran <121670156+KiranPolaki@users.noreply.github.com>
2024-05-07 00:54:31 +0530

    created skelton

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import { TopBar } from "./pages/TopBar";
+import { Links } from "./pages/Links";
+import { Intro } from "./pages/Intro";
+
+function App() {
+  return (
+    <>
+      <TopBar />
+      <Intro />
+      <Links />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/TopBar.jsx
@@ -0,0 +1,5 @@
+function TopBar() {
+  return <div></div>;
+}
+
+export { TopBar };
