a271429f72ab482fe53db5c84050297844a0e587
Polaki Sai Kiran <121670156+KiranPolaki@users.noreply.github.com>
2024-05-16 23:59:38 +0530

    update

---
+++ src/components/SampleTemplate.jsx
@@ -0,0 +1,13 @@
+import { Intro } from "./Intro";
+import { Links } from "./Links";
+
+function SampleTemplate() {
+  return (
+    <div className="sample-container">
+      <Intro />
+      <Links />
+    </div>
+  );
+}
+
+export { SampleTemplate };
--- src/pages/CreateTree.jsx
+++ src/pages/CreateTree.jsx
@@ -1,9 +1,11 @@
 // import { LinkTree } from "./LinkTree.jsx";
 import { Input } from "../components/Input.jsx";
+import { SampleTemplate } from "../components/SampleTemplate.jsx";
 function CreateTree() {
   return (
     <div className="create-continer">
       <Input />
+      <SampleTemplate />
     </div>
   );
 }
