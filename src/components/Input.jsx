0d5639224b1da4c88b22a21878772af19ed232f1
Polaki Sai Kiran <121670156+KiranPolaki@users.noreply.github.com>
2024-05-17 02:51:46 +0530

    input field structured

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,16 +1,26 @@
 function Input() {
   return (
     <div className="input-container">
-      <input className="input-box" type="text" placeholder="Name" />
-      <input className="input-box" type="text" placeholder="Bio" />
-      <input className="input-box" type="text" placeholder="experience" />
-      <div>
+      <div className="input-card">
+        <input className="input-box" type="text" placeholder="Name: Sukuna.." />
         <input
           className="input-box"
           type="text"
-          placeholder="links one at a time"
+          placeholder="Bio: I am king of curses.."
         />
-        <button className="links-btn">Add</button>
+        <input
+          className="input-box"
+          type="text"
+          placeholder="Experience: 100+years from Heian period..."
+        />
+        <div className="input-links">
+          <input
+            className="input-box-links"
+            type="text"
+            placeholder="Links: one at a time.."
+          />
+          <button className="links-btn">Add</button>
+        </div>
       </div>
     </div>
   );
--- src/components/SampleTemplate.jsx
+++ src/components/SampleTemplate.jsx
@@ -1,14 +1,19 @@
+/* eslint-disable no-unused-vars */
 // import { Intro } from "./Intro";
 // import { Links } from "./Links";
-import { SampleIntro } from "./SampleIntro";
-import { SampleLinks } from "./SampleLinks";
+import { SampleIntro } from "./SampleIntro.jsx";
+import { SampleLinks } from "./SampleLinks.jsx";
 
 function SampleTemplate() {
   return (
     <div className="sample-container">
-      <div className="card">
-        <SampleIntro />
-        <SampleLinks />
+      <div className="sample-card">
+        <img src="/left-arrow.png" alt="left-navigate" className="navigate" />
+        <div className="card">
+          <SampleIntro />
+          {/* <SampleLinks /> */}
+        </div>
+        <img src="/right-arrow.png" alt="right-navigate" className="navigate" />
       </div>
       {/* <Intro />
       <Links /> */}
