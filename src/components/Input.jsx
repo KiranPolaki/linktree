862af32b0a790428754eb8e3e76c254fe5c0953a
Polaki Sai Kiran <121670156+KiranPolaki@users.noreply.github.com>
2024-05-17 21:28:29 +0530

    input done

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,19 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Input({ sampleTree, setSampleTree, links, setLinks }) {
   const [linkName, setLinkName] = useState("");
   const [inputLink, setInputLink] = useState("");
-  function handleLinks() {}
+  const [bio, setBio] = useState({});
+  let id = 1;
+  function handleLinks() {
+    setLinks([...links, { id: id, name: linkName, link: inputLink }]);
+    id++;
+    setLinkName("");
+    setInputLink("");
+  }
+
+  function handleSave() {}
+
   return (
     <div className="input-container">
       <div className="input-card">
-        {console.log(typeof sampleTree.name)}
         <input
           className="input-box"
           type="text"
           maxLength={19}
           placeholder="Name: Sukuna.."
-          value={sampleTree.name}
+          value={sampleTree?.name}
           onChange={(e) =>
             setSampleTree({ ...sampleTree, name: e.target.value })
           }
@@ -28,16 +37,21 @@ function Input({ sampleTree, setSampleTree, links, setLinks }) {
             setSampleTree({ ...sampleTree, bio: e.target.value });
           }}
         />
-        <input
-          className="input-box"
-          type="text"
-          maxLength={35}
-          placeholder="Experience: 100+years from Heian period..."
-          value={sampleTree.experience}
-          onChange={(e) => {
-            setSampleTree({ ...sampleTree, experience: e.target.value });
-          }}
-        />
+        <div className="input-links">
+          <input
+            className="input-box-links"
+            type="text"
+            maxLength={35}
+            placeholder="Experience: 100+years from Heian period..."
+            value={sampleTree.experience}
+            onChange={(e) => {
+              setSampleTree({ ...sampleTree, experience: e.target.value });
+            }}
+          />
+          <button className="links-btn" onClick={handleSave}>
+            Save
+          </button>
+        </div>
       </div>
       <div className="links-card">
         <div className="input-links">
@@ -45,12 +59,15 @@ function Input({ sampleTree, setSampleTree, links, setLinks }) {
             type="text"
             className="input-box"
             placeholder="Display Name: Github"
-            // value={link}
+            value={linkName}
+            onChange={(e) => setLinkName(e.target.value)}
           />
           <input
             className="input-box-links"
             type="text"
             placeholder="Links: one at a time.."
+            value={inputLink}
+            onChange={(e) => setInputLink(e.target.value)}
           />
           <button className="links-btn" onClick={handleLinks}>
             Add
--- src/components/SampleLinks.jsx
+++ src/components/SampleLinks.jsx
@@ -1,8 +1,34 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable no-unused-vars */
-import { useState } from "react";
+import { useEffect, useState } from "react";
 function SampleLinks({ setLinks, links }) {
-  return <div className="sample-links-container"></div>;
+  return (
+    <div className="sample-links-container">
+      {console.log(links, "links")}
+      {links.length === 0 ? (
+        <></>
+      ) : (
+        links.map((link) => {
+          return (
+            <div className="sample-link" key={link?.id}>
+              <img
+                src={`${link?.link}/favicon.ico`}
+                width="25"
+                height="25"
+                className="img-link"
+              />
+              <a
+                href={link?.link}
+                target="_blank"
+                key="link?.id"
+                className="sample"
+              >
+                <p>{link?.name}</p>
+              </a>
+            </div>
+          );
+        })
+      )}
+    </div>
+  );
 }
 
 export { SampleLinks };
